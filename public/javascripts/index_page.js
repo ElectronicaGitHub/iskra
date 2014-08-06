6d1e891256568feb3d5ad5e8cd0d19f0ed9dd173
dimaastronom <dimasamsonov90@gmail.com>
2014-08-07 00:29:52 +0400

    Слайдер 1.0

--- public/javascripts/index_page.js
+++ public/javascripts/index_page.js
@@ -22,56 +22,11 @@ app.controller('Index', [ '$scope', '$http', function($scope, $http) {
 	}
 
 	var formatter = function() {
-
-		var inner_container = $('.inner_container'),
-			description = $('.description'),
-			news_title = $(),
-			initial_height=0;
-
-		// function setWidthTitle(container) {
-		// 	var c = ~~($(container).width()),
-		// 		e = ~~($(container).find('.img .news_title').width()),
-		// 		x = (c-e)/2;
-		// 	return x;
-		// }
-		// $.each(inner_container, function() {
-		// 	var b = $(this).find('.img .news_title');
-		// 	b.css('margin-left', setWidthTitle(this));
-		// 	setTimeout(function() {
-		// 		b.addClass('transitioner');
-		// 	})
-		// })
-		// inner_container.mouseenter(function() {
-		// 	var inner_container_hover = $('.inner_container:hover'),
-		// 		this_description = $(this).find(description),
-		// 		p = this_description.find('p'),
-		// 		a = ~~(inner_container_hover.find(description).find('p').height()),
-		// 		b = inner_container_hover.find(".img .news_title");
-		// 	initial_height=b.height();
-		// 	if(inner_container_hover.parent().hasClass('s11') || inner_container_hover.parent().hasClass('s22')) {
-		// 		b.css('height','-='+a);
-		// 	}
-		// 	if(inner_container_hover.parent().hasClass('s21')) {
-		// 		b.css('width','70%');
-		// 	}
-		// 	while (p.height()>this_description.height()) {
-		// 		p.css('font-size', '-=1px');
-		// 	}
-		// })
-		// inner_container.mouseleave(function() {
-		// 	var b = $(this).find(".img .news_title");
-		// 	if(inner_container.parent().hasClass('s21')) {
-		// 		b.css('width','100%');
-		// 	}
-		// 	b.height(initial_height);
-		// 	b.css('margin-left', setWidthTitle(this));
-		// })	
-
 		function sizeDescriptionHover(elem) {
 			var this_description = $(elem).find(description),
 				p = this_description.find('p');
 
-			while (p.height()>this_description.height()) {
+			while (p.height()>this_description.height() || p.width()>this_description.width()) {
 				p.css('font-size', '-=1px');
 			}
 		}
