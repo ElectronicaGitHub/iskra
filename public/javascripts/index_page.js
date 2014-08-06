08af348dec46a9802f3432169e3e84128c0f7fb2
Philip <molo4nik11@gmail.com>
2014-08-06 23:24:54 +0400

    Добавлено отображение всех новостей в админке

--- public/javascripts/index_page.js
+++ public/javascripts/index_page.js
@@ -1,11 +1,6 @@
 app.controller('Index', [ '$scope', '$http', function($scope, $http) {
 	
 	var page = 1;
-	var block_size = 4;
-	var sizes = [1,2,4];
-	var local_block = null;
-	var local_block_number = 0;
-	var init_number = 0;
 	$scope.feed_blocks = [];
 
 	$scope.init = function() {
@@ -19,55 +14,13 @@ app.controller('Index', [ '$scope', '$http', function($scope, $http) {
 				$scope.feeds = data;
 				console.log($scope.feeds)
 
-				fill_blocks();
+				$scope.feed_blocks = UTILS.blocks_former($scope.feeds, $scope.feed_blocks);
 			})
 			.error(function(data) {
 				console.log(data)
 			})
 	}
 
-	// for (var i=0; i<20; i++) {
-	// 	$scope.feeds.push({
-	// 		title : 'title number ' + i,
-	// 		size : sizes[~~(Math.random() * sizes.length)]
-	// 	})
-	// }
-	var fill_blocks = function() {
-		console.log('fillblocks init');
-		init_number ++;
-		for (var i in $scope.feeds) {
-			if (!local_block) {
-				local_block = {
-					id : ~~(Math.random() * 9999),
-					feed_elements : []
-				}
-			}
-			if (local_block_number < block_size) {
-				if ($scope.feeds[i].size <= (block_size-local_block_number)) {
-					local_block.feed_elements.push($scope.feeds[i]);
-					local_block_number += $scope.feeds[i].size;
-					$scope.feeds.splice(i,1);
-					i--;
-				}
-			} else {
-				sort_way = Math.random() > .5;
-				local_block.feed_elements.sort(function(a,b) {
-					return sort_way ? a.size-b.size : b.size-a.size
-				})
-				$scope.feed_blocks.push(local_block);
-				local_block = null;
-				local_block_number = 0;
-			}
-		}
-		if ($scope.feeds.length != 0 && init_number < 10) {
-			fill_blocks();
-		}
-		console.log('feeds', $scope.feeds);
-		console.log('blocks', $scope.feed_blocks);
-		setTimeout(function() {
-			formatter();
-		})
-	}
 	var formatter = function() {
 
 		var inner_container = $('.inner_container'),
---
+++ public/javascripts/utils.js
@@ -0,0 +1,52 @@
+var UTILS = (function() {
+
+	var block_size = 4,
+	local_block = null,
+	local_block_number = 0,
+	init_number = 0;
+
+	return {
+		blocks_former : function(feeds, blocks) {
+			console.warn('UTILS BLOCK FORMER')
+			init_number ++;
+			for (var i in feeds) {
+				if (!local_block) {
+					local_block = {
+						id : ~~(Math.random() * 9999),
+						feed_elements : []
+					}
+				}
+				if (local_block_number < block_size) {
+					if (feeds[i].size <= (block_size-local_block_number)) {
+						local_block.feed_elements.push(feeds[i]);
+						local_block_number += feeds[i].size;
+						feeds.splice(i,1);
+						i--;
+					}
+				} else {
+					sort_way = Math.random() > .5;
+					local_block.feed_elements.sort(function(a,b) {
+						return sort_way ? a.size-b.size : b.size-a.size
+					})
+					blocks.push(local_block);
+					local_block = null;
+					local_block_number = 0;
+				}
+			}
+			if (feeds.length != 0 && init_number < 10) {
+				this.blocks_former(feeds, blocks);
+			}
+			console.log('feeds', feeds);
+			console.log('blocks', blocks);
+			setTimeout(function() {
+				if (formatter) {
+					formatter();
+				} else {
+					console.warn('text formatter not allowed');
+				}
+			})
+
+			return blocks;
+		}
+	}
+})();
