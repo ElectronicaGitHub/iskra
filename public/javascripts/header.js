cafc0adf17da8d512c0200e6a58bc9191bfab5d9
Philip <molo4nik11@gmail.com>
2014-08-25 02:11:43 +0400

    переход делается реально на ту новость которая ссылка

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -3,16 +3,22 @@ tk.controller('HeaderCtrl', ['$scope', '$rootScope', '$location', 'localStorageS
 
 	//init page 
 	$rootScope.news_type = $scope.news_type = localStorageService.get('tvoyKosmos_MODE');
-	// $rootScope.news_type = $scope.news_type = localStorageService.get('tvoyKosmos_MODE') || 'normal';
 	$rootScope.$watch('news_type', function(value) {
-		console.log('news type header watcher')
-		if (value) $location.path(value);
-		$scope.news_type = value ? value : '';
+		console.log('news type header watcher');
+		search_value = $location.path().slice(1) || null;
+		if (value && search_value && value != search_value) {
+			$location.path(search_value);
+			$rootScope.news_type = $scope.news_type = search_value;
+		} else {
+			$location.path(value);
+			$scope.news_type = value;	
+		}
 	})
 	$scope.toggle_news_type = function() {
 		$scope.news_type = $scope.news_type == 'normal' ? 'special' : 'normal';
 		$rootScope.news_type  = $scope.news_type;
 		localStorageService.set('tvoyKosmos_MODE', $scope.news_type);
+		$location.path($scope.news_type);
 	}
 	$scope.goToRoot = function() {
 		window.location.href = '/';
