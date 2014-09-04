f26a8a3f9e22e4f04911ac7b99723a16abd6b597
Philip <molo4nik11@gmail.com>
2014-09-04 19:32:05 +0400

    убраны баг с хедером

--- public/javascripts/misc.js
+++ public/javascripts/misc.js
@@ -8,10 +8,8 @@ $(document).ready(function() {
 	$(document).on('scroll', function() {
 		if ($(window).scrollTop() >= 150) {
 			$('.mini-top').addClass('showed');
-			$('.top').addClass('showed');
 		} else {
 			$('.mini-top').removeClass('showed');
-			$('.top').removeClass('showed');
 		}
 	})
 })
